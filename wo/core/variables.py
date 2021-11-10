3853f2c96c521a924961f895faf5c9e184742ee1
Justin Givens <jgivens@imageinabox.com>
2021-11-10 09:44:02 -0600

    moving away from SITEURL

--- wo/core/variables.py
+++ wo/core/variables.py
@@ -14,7 +14,7 @@ class WOVar():
     """Intialization of core variables"""
 
     # WordOps version
-    wo_version = "3.14.5.8"
+    wo_version = "3.14.5.9"
     # WordOps packages versions
     wo_wp_cli = "2.4.0"
     wo_adminer = "4.7.5"
